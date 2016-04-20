4da166e536a9fdda436a6817b1432bcf07d9c5c1
timor2015 <3209742516@qq.com>
2016-04-20 21:17:26 +0800

    swiper滑动结构搭建完成

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+setTimeout(function(){
+	var mySwiper = new Swiper ('.swiper-container', {
+	    direction: 'vertical',
+	    loop: true,
+	    
+	    // 如果需要分页器
+	    pagination: '.swiper-pagination',
+	});
+}, 300);
+        
+
+
